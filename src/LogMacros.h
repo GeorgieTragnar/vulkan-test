ac02ed7d50a85e1b1316198c6f2ab8c119e204d5
harcar <harcar.jiri@gmail.com>
2024-09-29 17:51:07 +0200

    modified logging colors and added trace log lvl

--- src/LogMacros.h
+++ src/LogMacros.h
@@ -12,14 +12,30 @@
 #include <thread>   // for std::this_thread::get_id
 
 // ANSI color codes
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-#define WHITE   "\033[37m"
+#define RESET         "\033[0m"
+#define RED           "\033[31m"
+#define GREEN         "\033[32m"
+#define YELLOW        "\033[33m"
+#define BLUE          "\033[34m"
+#define MAGENTA       "\033[35m"
+#define CYAN          "\033[36m"
+#define WHITE         "\033[37m"
+
+#define BOLD_WHITE    "\033[1;37m"  // Bold white
+#define BOLD_YELLOW   "\033[1;33m"  // Bold yellow
+#define BOLD_RED      "\033[1;31m"  // Bold red
+#define DIM_WHITE     "\033[2;37m"
+
+// Background colors
+#define BG_BLACK      "\033[40m"
+#define BG_RED        "\033[41m"
+#define BG_GREEN      "\033[42m"
+#define BG_YELLOW     "\033[43m"
+#define BG_BLUE       "\033[44m"
+#define BG_MAGENTA    "\033[45m"
+#define BG_CYAN       "\033[46m"
+#define BG_WHITE      "\033[47m"
+
 
 // Max length of a log line before wrapping
 const int LOG_LINE_LENGTH = 232; // You can adjust this value
@@ -120,7 +136,7 @@ inline std::string getThreadId() {
 }
 
 // Base logging macro with file name and line number at the end, supporting printf-style formatting and colors
-#define LOG_COLOR(level, color, format, ...) \
+#define LOG_COLOR(level, color, bgColor, format, ...) \
 	do { \
 		std::string subFormat = substituteFormatSpecifiers(format, ##__VA_ARGS__); \
 		std::string formattedMsg = formatString(subFormat.c_str(), ##__VA_ARGS__); \
@@ -129,7 +145,7 @@ inline std::string getThreadId() {
 		int metadataLength = 27 + fileInfo.size() + 1;  /* timestamp + " | " + thread_id (max 19 chars) + " | " + log level + " | " */ \
 		int availableSpace = LOG_LINE_LENGTH - metadataLength; \
 		std::vector<std::string> wrappedLines = wrapMessage(formattedMsg, availableSpace); \
-		std::cout << color << dateTime << "|" << getThreadId() << "|" \
+		std::cout << color << bgColor << dateTime << "|" << getThreadId() << "|" \
 			<< level << "| " << wrappedLines[0] << " " << fileInfo; \
 		for (size_t i = 1; i < wrappedLines.size(); ++i) { \
 			std::cout << "\n" << std::string(27 + 1, ' ') << wrappedLines[i]; \
@@ -137,17 +153,9 @@ inline std::string getThreadId() {
 		std::cout << RESET << std::endl; \
 	} while (0)
 
-// Info log with green color
-#define LOG_INFO(format, ...) LOG_COLOR("INFO ", GREEN, format, ##__VA_ARGS__)
-
-// Warning log with yellow color
-#define LOG_WARNING(format, ...) LOG_COLOR("WARN ", YELLOW, format, ##__VA_ARGS__)
-
-// Error log with red color
-#define LOG_ERROR(format, ...) LOG_COLOR("ERROR", RED, format, ##__VA_ARGS__)
-
-// Debug log with cyan color
-#define LOG_DEBUG(format, ...) LOG_COLOR("DEBUG", CYAN, format, ##__VA_ARGS__)
-
-// Critical log with magenta color
-#define LOG_CRITICAL(format, ...) LOG_COLOR("CRIT ", MAGENTA, format, ##__VA_ARGS__)
+#define LOG_TRACE(format, ...) LOG_COLOR("TRACE", DIM_WHITE, BG_BLACK, format, ##__VA_ARGS__)
+#define LOG_INFO(format, ...) LOG_COLOR("INFO ", GREEN, BG_BLACK, format, ##__VA_ARGS__)
+#define LOG_DEBUG(format, ...) LOG_COLOR("DEBUG", CYAN, BG_BLACK, format, ##__VA_ARGS__)
+#define LOG_WARNING(format, ...) LOG_COLOR("WARN ", BOLD_YELLOW, BG_BLACK, format, ##__VA_ARGS__)
+#define LOG_ERROR(format, ...) LOG_COLOR("ERROR", BOLD_RED, BG_BLACK, format, ##__VA_ARGS__)
+#define LOG_CRITICAL(format, ...) LOG_COLOR("CRIT ", BOLD_WHITE, BG_RED, format, ##__VA_ARGS__)
