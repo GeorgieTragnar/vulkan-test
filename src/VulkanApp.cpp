e751405979ebbf6b9cac3c6032338e0653434dc6
harcar <harcar.jiri@gmail.com>
2024-10-04 19:33:10 +0200

    fixed forgotten setup for composite alpha and swapimageinfo

--- src/VulkanApp.cpp
+++ src/VulkanApp.cpp
@@ -30,6 +30,7 @@ VulkanApp::~VulkanApp()
 	if (_enableValidationLayers) {
 		destroyDebugUtilsMessengerEXT();
 	}
+	vkDestroySwapchainKHR(_device, _swapChain, nullptr);
 	vkDestroySurfaceKHR(_instance, _surface, nullptr);
     vkDestroyDevice(_device, nullptr);
 	vkDestroyInstance(_instance, nullptr);
@@ -426,6 +427,13 @@ void VulkanApp::createSwapChain()
 	createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
 	createInfo.surface = _surface;
 
+	createInfo.minImageCount = imageCount;
+	createInfo.imageFormat = surfaceFormat.format;
+	createInfo.imageColorSpace = surfaceFormat.colorSpace;
+	createInfo.imageExtent = extent;
+	createInfo.imageArrayLayers = 1;
+	createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
 	QueueFamilyIndices indices = findQueueFamilies(_physicalDevice);
 	uint32_t queueFamilyIndices[] = {indices._graphicsFamily.value(), indices._presentFamily.value()};
 
@@ -443,6 +451,7 @@ void VulkanApp::createSwapChain()
 	}
 
 	createInfo.preTransform = swapChainSupport._capabilities.currentTransform;
+	createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
 
 	createInfo.presentMode = presentMode;
 	createInfo.clipped = VK_TRUE;
