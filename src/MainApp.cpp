872d57495cc7e7754651803d9e63b04ffc9fcb57
harcar <harcar.jiri@gmail.com>
2024-10-12 14:38:38 +0200

    added avg frame display log after 50k frames

--- src/MainApp.cpp
+++ src/MainApp.cpp
@@ -27,9 +27,29 @@ void MainApp::run()
 
 void MainApp::mainLoop() 
 {
+	using clock = std::chrono::high_resolution_clock;
+	auto startTime = clock::now();
+	int frameCount = 0;
+	const int frameLimit = 50000; // Measure after 50000 frames
+
 	while (Window::Instance().loopStep())
 	{
 		VulkanApp::Instance().drawFrame();
+		frameCount++;
+
+		if (frameCount == frameLimit) {
+			auto endTime = clock::now();
+			auto duration = std::chrono::duration_cast<std::chrono::seconds>(endTime - startTime).count();
+
+			if (duration > 0) {
+				float averageFps = frameLimit / static_cast<float>(duration);
+				LOG_TRACE("Average FPS over {} frames: {}", frameLimit, averageFps);
+			}
+
+			// Reset frame counter and timer
+			frameCount = 0;
+			startTime = clock::now();
+		}
 	}
 
 	VulkanApp::Instance().waitIdle();
