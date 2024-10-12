17be14a5151b7441bd19d498d30fb618d007ccb5
harcar <harcar.jiri@gmail.com>
2024-10-12 14:29:29 +0200

    added mipmaps generation and usage

--- src/VulkanApp.h
+++ src/VulkanApp.h
@@ -134,16 +134,17 @@ protected:
 	void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
 	void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
 	void updateUniformBuffer(uint32_t currentImage);
-	void createImage(uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, 
-		VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
 	uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
 
 	VkCommandBuffer beginSingleTimeCommands();
 	void endSingleTimeCommands(VkCommandBuffer commandBuffer);
-	void transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout);
+	void transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels);
 	void copyBufferToImage(VkBuffer buffer, VkImage image, uint32_t width, uint32_t height);
 
-	VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags);
+	void generateMipmaps(VkImage image, VkFormat imageFormat, int32_t texWidth, int32_t texHeight, uint32_t mipLevels);
+	void createImage(uint32_t width, uint32_t height, uint32_t mipLevels, VkFormat format, VkImageTiling tiling, 
+		VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
+	VkImageView createImageView(VkImage image, VkFormat format, VkImageAspectFlags aspectFlags, uint32_t mipLevels);
 	VkShaderModule createShaderModule(const std::vector<char>& code);
 
 	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
@@ -155,25 +156,25 @@ protected:
 	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
 
 
-	const bool _enableValidationLayers;
-	const std::vector<const char*> _validationLayers = {
+	const bool						_enableValidationLayers;
+	const std::vector<const char*>	_validationLayers = {
 		"VK_LAYER_KHRONOS_validation"
 	};
 
-	const std::vector<const char*> _deviceExtensions = {
+	const std::vector<const char*>	_deviceExtensions = {
 		VK_KHR_SWAPCHAIN_EXTENSION_NAME
 	};
 
-	const std::string MODEL_PATH = "models/viking_room.obj";
-	const std::string TEXTURE_PATH = "textures/viking_room.png";
+	const std::string				MODEL_PATH = "models/viking_room.obj";
+	const std::string				TEXTURE_PATH = "textures/viking_room.png";
 
-	std::vector<Vertex> _vertices;
-	std::vector<uint32_t> _indices;
+	std::vector<Vertex>				_vertices;
+	std::vector<uint32_t>			_indices;
 
-	VkDebugUtilsMessengerEXT _debugMessenger;
+	VkDebugUtilsMessengerEXT		_debugMessenger;
 
-	std::atomic<bool> _frameBufferResized;
-	uint32_t _currentFrame;
+	std::atomic<bool>				_frameBufferResized;
+	uint32_t						_currentFrame;
 
 	std::vector<VkSemaphore>		_imageAvailableSemaphores;
 	std::vector<VkSemaphore>		_renderFinishedSemaphores;
@@ -182,6 +183,7 @@ protected:
 	VkImage 						_depthImage;
 	VkDeviceMemory 					_depthImageMemory;
 	VkImageView 					_depthImageView;
+	uint32_t						_textureMipLevels;
 	VkSampler						_textureSampler;
 	VkImageView						_textureImageView;
 	VkImage							_textureImage;
